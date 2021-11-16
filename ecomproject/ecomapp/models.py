7cf9b6542593004f7b8f9ca2bf6922ac59d5fadd
svvetal <svvetal14@gmail.com>
2021-11-16 12:45:36 +0530

    Models

--- ecomproject/ecomapp/models.py
+++ ecomproject/ecomapp/models.py
@@ -7,3 +7,65 @@ class Customer(models.Model):
     full_name = models.CharField(max_length=200)
     address = models.CharField(max_length=200 , null=True , blank=True)
     joined_on = models.DateTimeField(auto_now_add=True)
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=100 , unique=True)
+    category = models.ForeignKey(Category , on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="products")
+    marked_price = models.PositiveBigIntegerField()
+    selling_price = models.PositiveIntegerField()
+    description = models.TextField()
+    warranty = models.CharField(max_length=300 , null=True, blank=True)
+    view_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL , null=True , blank=True)
+    total = models.PositiveBigIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "Cart : " + str(self.id)
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey(Cart , on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rate = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+    subtotal = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "Cart : " + str(self.cart.id) + "CartProduct" + str(self.id)
+
+ORDER_STATUS = (
+    ("Order Received" , "Order Received"),
+    ("Order Processing" , "Order Processing"),
+    ("Order on the Way" , "Order on the Way"),
+    ("Order Completed" , "Order Completed"),
+    ("Order Cancelled" , "Order Cancelled"),
+)
+
+class Order(models.Model):
+    cart = models.OneToOneField(Cart, on_delete=models.CASCADE)
+    ordered_by = models.CharField(max_length=200)
+    shipping_address = models.CharField(max_length=200)
+    mobile = models.CharField(max_length=10)
+    email = models.EmailField(null=True, blank=True)
+    subtotal = models.PositiveIntegerField()
+    discount = models.PositiveIntegerField()
+    total = models.PositiveIntegerField()
+    order_status = models.CharField(max_length=50 , choices=ORDER_STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "Order : " + str(self.id)
