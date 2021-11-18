2105a39a0b0a65d67964220ef802b6b47986512a
INGENIOUS <sagarabhyankar18@gmail.com>
2021-11-18 17:18:55 +0530

    Revert "selffix"
    This reverts commit 74fbf0b5308adc3a0b85ce6538415fab8eb2afeb.

--- ecomproject/ecomapp/forms.py
+++ ecomproject/ecomapp/forms.py
@@ -1,15 +1,20 @@
 from django import forms
-from .models import Customer
+from .models import Customer, Seller
 from django.contrib.auth.models import User
-from django.forms import ModelForm
 
 class CustomerRegistrationForm(forms.ModelForm):
-    username = forms.CharField(widget=forms.TextInput())
-    password = forms.CharField(widget=forms.PasswordInput())
-    email = forms.CharField(widget=forms.EmailInput())
+    username = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class' : 'form-control'}))
     class Meta:
         model = Customer
         fields = ["username" , "password" , "email" , "full_name" , "address"]
+
+    def __init__(self , *args , **kwargs):
+        super (CustomerRegistrationForm , self).__init__(*args , **kwargs)
+
+        self.fields["full_name"].widget.attrs['class'] = 'form-control'
+        self.fields["address"].widget.attrs['class'] = 'form-control'
 
     def clean_username(self):
         uname = self.cleaned_data.get("username")
@@ -24,3 +29,32 @@ class CustomerLoginForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ["username" , "password"]
+
+    def __init__(self , *args , **kwargs):
+        super (CustomerLoginForm , self).__init__(*args , **kwargs)
+
+        self.fields["username"].widget.attrs['class'] = 'form-control'
+        self.fields["password"].widget.attrs['class'] = 'form-control'
+
+class SellerRegistrationForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class' : 'form-control'}))
+    class Meta:
+        model = Seller
+        fields = ["username" , "password" , "email" , "full_name" , "address" , "shop_name" , "shop_id"]
+
+    def __init__(self , *args , **kwargs):
+        super (SellerRegistrationForm , self).__init__(*args , **kwargs)
+
+        self.fields["full_name"].widget.attrs['class'] = 'form-control'
+        self.fields["address"].widget.attrs['class'] = 'form-control'
+        self.fields["shop_name"].widget.attrs['class'] = 'form-control'
+        self.fields["shop_id"].widget.attrs['class'] = 'form-control'
+
+    def clean_username(self):
+        uname = self.cleaned_data.get("username")
+        if User.objects.filter(username = uname).exists():
+            raise forms.ValidationError("Customer with this Username already Exists")
+
+        return uname
--- ecomproject/ecomapp/urls.py
+++ ecomproject/ecomapp/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 app_name = "ecomapp"
 urlpatterns = [
-    path("", HomeView.as_view() , name="landing"),
+    path("", HomeView.as_view() , name="home"),
     path('About/' , AboutView.as_view() , name = "about"),
     path('Contact/' , ContactView.as_view() , name = "contact"),
     path('register/' , CustomerRegistrationView.as_view() , name = "customerregistration"),
@@ -11,4 +11,11 @@ urlpatterns = [
     path('login/' , CustomerLoginView.as_view() , name = "customerLogin"),
     path('all-products/' , AllProductsView.as_view() , name = "allproducts"),
     path('shop/' , ShopView.as_view() , name = "shop"),
+    path('register/register-customer/' , CustormerRegisterView.as_view() , name = "customerregister"),
+    path('register/register-seller/' , SellerRegisterView.as_view() , name = "sellerregister"),
+    path('search/',SearchView.as_view(),name="search"),
+    path('product/<slug:slug>/',ProductDetailView.as_view(),name="productdetailview"),
+    path('add-to-cart-<int:pro_id>/',AddToCartView.as_view(),name="addtocart"),
+    path('cart/',CartView.as_view(),name="cart"),
+    path('seller-admin/' , SellerAdminView.as_view() , name = "selleradmin")
 ]
