5b8837cbc13a2ac6de5b2b3161c98fd0c038e6de
INGENIOUS <sagarabhyankar18@gmail.com>
2021-11-18 21:11:01 +0530

    completed adding to cart

--- ecomproject/ecomapp/views.py
+++ ecomproject/ecomapp/views.py
@@ -162,3 +162,35 @@ class CartView(TemplateView):
             context['cart'] = None
         
         return context
+
+class ManageCartView(View):
+    def get(self , request , *args , **kwargs):
+        
+        cp_id = self.kwargs.get("cp_id")
+        action = request.GET.get("action")
+        cp_obj = CartProduct.objects.get(id=cp_id)
+        cart_obj = cp_obj.cart
+
+        if action == "inc":
+            cp_obj.quantity += 1
+            cp_obj.subtotal += cp_obj.rate
+            cp_obj.save()
+            cart_obj.total += cp_obj.rate
+            cart_obj.save()
+        elif action == "dec":
+            cp_obj.quantity -= 1
+            cp_obj.subtotal -= cp_obj.rate
+            cp_obj.save()
+            cart_obj.total -= cp_obj.rate
+            cart_obj.save()
+            if cp_obj.quantity == 0:
+                cp_obj.delete()
+
+        elif action == "rem":
+            cart_obj.total -= cp_obj.subtotal
+            cart_obj.save()
+            cp_obj.delete()
+        else:
+            pass
+        return redirect("ecomapp:cart")
+
